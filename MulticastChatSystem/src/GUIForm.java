a62c89ce2e2f8c016bfb023384f68760b24fe037
ahuynh359 <ahuynh359@gmail.com>
2023-11-14 02:44:15 +0700

    Add UI

---
+++ MulticastChatSystem/src/GUIForm.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class GUIForm {
+    private JTextField tfGroupName;
+    private JTextField tfPort;
+    private JTextField tfName;
+    private JPanel panelParent;
+    private JPanel panelGroupPort;
+    private JPanel panelName;
+    private JLabel lbGroupName;
+    private JLabel lbPort;
+    private JLabel lbName;
+    private JPanel panelMessage;
+    private JTextArea taMessage;
+    private JLabel lbMessage;
+    private JPanel panelCrypto;
+    private JPanel panelButton;
+    private JPanel panelChat;
+    private JTextField tfMessage;
+    private JButton btnSend;
+    private JButton btnJoinChat;
+    private JButton btnLeaveChat;
+    private JButton btnExit;
+    private JCheckBox cmbEncrypt;
+    private JTextField tfKey;
+    private JLabel lbKey;
+}
