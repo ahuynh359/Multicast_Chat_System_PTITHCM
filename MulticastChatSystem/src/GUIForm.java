3aa6b4feec90d2396044b97b2154d4f84b8b4ab5
ahuynh359 <ahuynh359@gmail.com>
2023-11-15 03:30:01 +0700

    add function

--- MulticastChatSystem/src/GUIForm.java
+++ MulticastChatSystem/src/GUIForm.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class GUIForm {
     private JTextField tfGroupName;
@@ -24,4 +28,206 @@ public class GUIForm {
     private JCheckBox cmbEncrypt;
     private JTextField tfKey;
     private JLabel lbKey;
+    private JLabel lbSendMessage;
+
+    private MulticastSender multicastSender;
+    private MulticastListener multicastListener;
+    private ArrayList<String> messageArray;
+    private int currentNumMessage;
+
+    public GUIForm() {
+
+        init();
+        handleEvent();
+
+
+
+    }
+
+    private void init() {
+        messageArray = new ArrayList<>();
+        messageArray.add("Join chat to begin conversation");
+        taMessage.setText("Join chat to begin conversation");
+
+        currentNumMessage = 0;
+
+        //Send
+        multicastSender = new MulticastSender();
+
+        //Listen
+        String groupName = tfGroupName.getText();
+        int port = Integer.parseInt(tfPort.getText());
+        String name = tfName.getText();
+        multicastListener = new MulticastListener(name, groupName, port, new ArrayList<String>(), currentNumMessage);
+
+
+        //Every 7s update chat box
+        UpdateSystem updateSystem = new UpdateSystem();
+        Timer timer = new Timer(700, updateSystem);
+        timer.start();
+
+
+    }
+
+    private void handleEvent() {
+        //Btn Join Chat
+        btnJoinChat.addActionListener(e -> {
+            if (handleErrorBeforeConver()) {
+
+                btnJoinChat.setEnabled(false);
+
+                //Get info from text field
+                String groupName = tfGroupName.getText();
+                int port = Integer.parseInt(tfPort.getText());
+                String name = tfName.getText();
+
+                //Send and receive message
+                multicastSender.sendMessage("", groupName, port);
+                multicastListener = new MulticastListener(name, groupName, port, messageArray, currentNumMessage);
+                multicastListener.start();
+
+                //Current Number
+                currentNumMessage = multicastListener.totalMessages();
+
+                //Set UI
+                btnSend.setEnabled(true);
+                btnLeaveChat.setEnabled(true);
+                togglePanelSendMessage(true);
+                togglePanelEncrypt(true);
+                togglePanelIp(false);
+                togglePanelName(false);
+
+
+            }
+
+        });
+
+        //Btn Leave Chat
+        btnLeaveChat.addActionListener(e -> {
+            btnJoinChat.setEnabled(true);
+
+            try {
+                multicastListener.stopProgram();
+                String mess = tfName.getText() + " is disconnecting from chat! \n";
+
+
+                multicastSender.sendMessage(mess, tfGroupName.getText(), Integer.parseInt(tfPort.getText()));
+                multicastListener.join(1000);
+            } catch (Exception err){
+                err.printStackTrace();
+            }
+
+            //Set UI
+            btnSend.setEnabled(false);
+            btnLeaveChat.setEnabled(false);
+            togglePanelSendMessage(false);
+            togglePanelEncrypt(false);
+            togglePanelIp(true);
+            togglePanelName(true);
+        });
+
+        //Btn send message
+        btnSend.addActionListener(e -> {
+            String mess = tfName.getText() + ": " + tfMessage.getText();
+            multicastSender.sendMessage(mess,tfGroupName.getText(),Integer.parseInt(tfPort.getText()));
+            tfMessage.setText("");
+        });
+
+        //Btn Exit
+        btnExit.addActionListener(e -> {
+            if(btnLeaveChat.isEnabled()){
+                multicastListener.stopProgram();
+                try {
+                    multicastListener.join(500);
+                }catch (Exception err){
+                    err.printStackTrace();
+                }
+                String mess = tfName.getText() + " has exited chart!";
+                multicastSender.sendMessage(mess,tfGroupName.getText(),Integer.parseInt(tfPort.getText()));
+            }
+            System.exit(0);
+        });
+    }
+
+    private boolean handleErrorBeforeConver() {
+
+        if (tfGroupName.getText().isEmpty()) {
+            createErrorPane("Do not leave group name empty");
+            return false;
+        }
+
+        if (tfPort.getText().isEmpty()) {
+            createErrorPane("Do not leave port empty");
+            return false;
+        }
+
+        if (tfName.getText().isEmpty()) {
+            createErrorPane("Do not leave name empty");
+            return false;
+        }
+        return true;
+
+    }
+
+    private void createErrorPane(String errorMessage) {
+        JOptionPane.showMessageDialog(
+                panelParent,
+                errorMessage,
+                "Error",
+                JOptionPane.ERROR_MESSAGE
+        );
+    }
+
+    private void togglePanelName(Boolean toggle) {
+        lbName.setEnabled(toggle);
+        tfName.setEnabled(toggle);
+    }
+
+    private void togglePanelIp(Boolean toggle) {
+        lbPort.setEnabled(toggle);
+        lbGroupName.setEnabled(toggle);
+        tfPort.setEnabled(toggle);
+        tfGroupName.setEnabled(toggle);
+    }
+
+    private void togglePanelSendMessage(Boolean toggle) {
+        lbSendMessage.setEnabled(toggle);
+        tfMessage.setEnabled(toggle);
+
+    }
+
+    private void togglePanelEncrypt(Boolean toggle) {
+        lbKey.setEnabled(toggle);
+        cmbEncrypt.setEnabled(toggle);
+        tfKey.setEnabled(toggle);
+    }
+
+    public void startGUI() {
+        JFrame frame = new JFrame("Multicast Chat Room");
+        frame.setSize(800, 600);
+        frame.add(panelParent);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    private class UpdateSystem implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            try {
+
+                messageArray = multicastListener.getMessages();
+                int numMessage = multicastListener.totalMessages();
+                while (currentNumMessage != numMessage) {
+                    String mess = messageArray.get(currentNumMessage++);
+                    taMessage.setText(taMessage.getText() + mess + "\n");
+                }
+            } catch (Exception err) {
+                err.printStackTrace();
+            }
+        }
+    }
 }
---
+++ MulticastChatSystem/src/MulticastListener.java
@@ -0,0 +1,74 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.NetworkInterface;
+import java.util.ArrayList;
+
+public class MulticastListener extends Thread {
+
+    private final byte[] buffer = new byte[256];
+    private String userName;
+    private final String groupName;
+
+    private final int port;
+    private ArrayList<String> messages;
+    private volatile Boolean running;
+
+    private volatile int numOfMessage;
+
+    public MulticastListener(String userName, String groupName, int port, ArrayList<String> messages, int numOfMessage) {
+        this.userName = userName;
+        this.groupName = groupName;
+        this.port = port;
+        this.messages = messages;
+        this.numOfMessage = numOfMessage;
+        this.running = true;
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            InetAddress group = InetAddress.getByName(groupName);
+            MulticastSocket socket = new MulticastSocket(port);
+            socket.joinGroup(group);
+
+            messages.add("\n\n" + userName + " is connected to chat at " + groupName + "\n");
+            numOfMessage++;
+            while (running) {
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+
+                String message = new String(packet.getData(), 0, packet.getLength());
+
+                messages.add(message);
+                numOfMessage++;
+
+
+            }
+            socket.leaveGroup(group);
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized ArrayList<String> getMessages() {
+        return messages;
+    }
+
+    public synchronized int totalMessages() {
+        return numOfMessage;
+    }
+
+    public synchronized void stopProgram() {
+        running = false;
+    }
+
+    public synchronized void addMessage(String message) {
+        messages.add(message);
+        numOfMessage++;
+    }
+}
